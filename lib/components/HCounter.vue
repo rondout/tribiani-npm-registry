b1e2f1c9a6ddfa5a2906f661480075c22dd52eae
shufei.han <shufei.han@gl-inet.com>
2024-05-07 11:43:35 +0800

    chore: 基础搭建

---
+++ lib/components/HCounter.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Description: 
+ * @Author: shufei.han
+ * @LastEditors: shufei.han
+ * @Date: 2024-05-07 10:58:06
+ * @LastEditTime: 2024-05-07 11:27:56
+-->
+<template>
+    <button @click="add">+</button>
+    <h1>
+        HCounter: {{count}}
+    </h1>
+    <button @click="minus">-</button>
+</template> 
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{defaultValue?:number}>()
+
+const count = ref(props.defaultValue || 0)
+
+const add = () => count.value++
+
+const minus = () => count.value--
+</script> 
+
+<style lang="scss" scoped></style>
---
+++ lib/main.ts
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Author: shufei.han
+ * @LastEditors: shufei.han
+ * @Date: 2024-05-07 10:54:49
+ * @LastEditTime: 2024-05-07 10:58:58
+ */
+export default { name: "main.ts" };
+
+export { default as HCounter } from "./components/HCounter.vue";
